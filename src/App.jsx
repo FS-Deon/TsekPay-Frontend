e7dc196f7c0c55a4c32a379feacbdc29bf15bad3
ags0522 <antoinette@fullsuite.ph>
2024-01-05 16:03:06 +0800

    Fixed App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 
+import Login from "/src/pages/Login.jsx"
+import Dashboard from "/src/pages/Dashboard.jsx"
+import Register from "/src/pages/Register.jsx"
+import UploadRecord from "/src/pages/UploadRecord.jsx"
+import NonRecurring from "/src/pages/NonRecurring.jsx"
+import ActivityLogs from "/src/pages/ActivityLogs.jsx"
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/upload-record" element={<UploadRecord/>} />
+          <Route path="/non-recurring" element={<NonRecurring/>} />
+          <Route path="/activity-logs" element={<ActivityLogs/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+       
     </>
   )
 }
