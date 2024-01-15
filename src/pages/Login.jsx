48d5122ddbfbf9a14b502e1717a1699d48a49b52
angelo-artiaga <angelo.artiaga@gmail.com>
2024-01-15 14:08:03 +0800

    update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,87 +9,62 @@ function Login() {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-20 w-auto"
-            src="../Fs-logo.png"
-            alt="FullSuite Logo"
-          />
-          <h2 className="mt-0 mb-3 font-sans text-center text-l leading-9 tracking-tight text-gray-900">
-            FullSuite
-          </h2>
-          <h2 className="mt-0 font-mono text-center text-xl font-bold leading-9 tracking-tight text-gray-900">
-            HRI System: TSEKPAY
-          </h2>
-        </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-left text-sm font-medium leading-6 text-gray-900"
-            >
-              Email Address
-            </label>
-            <div className="mt-2">
+      <div class="w-screen min-h-screen">
+        <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex flex-col shadow lg:w-[450px] w-[350px]">
+            <h1 className="text-[20px] font-medium mx-8 my-6">Login</h1>
+            <div class="relative z-0 mx-10 my-5">
               <input
-                id="email"
-                name="email"
                 type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0097B2] sm:text-sm sm:leading-6 p-2"
+                id="floating_standard"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
               />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-left justify-between">
               <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                for="floating_standard"
+                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
               >
-                Password
+                Enter your email address
               </label>
             </div>
-            <div className="mt-2">
+            <div class="relative z-0 mx-10 my-5">
               <input
-                id="password"
-                name="password"
-                type={showPassword ? "text" : "password"}
-                value={password}
-                autoComplete="current-password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0097B2] sm:text-sm sm:leading-6 p-2"
+                type="email"
+                id="floating_standard"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
               />
-              <div className="flex items-center mt-4 mb-4">
+              <label
+                for="floating_standard"
+                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+              >
+                Enter your password
+              </label>
+            </div>
+
+            <p className="text-[13px] text-[#002C74] mx-10">
+              Forgot your password?
+            </p>
+            <div className="flex flex-col justify-center items-center py-5">
+              <div class="flex items-center mb-3">
                 <input
-                  className="checkbox checkbox-sm mr-2"
-                  id="check"
+                  id="default-checkbox"
                   type="checkbox"
-                  value={showPassword}
-                  onChange={() => setShowPassword((prev) => !prev)}
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 focus:ring-2 dark:bg-gray-700"
                 />
-                <label className="text-sm" htmlFor="check">
-                  Show Password
+                <label for="default-checkbox" class="ms-2 text-[13px]">
+                  Stay signed in for a week
                 </label>
               </div>
+              <button className="btn bg-[#5D93F0] w-[60%] btn-sm text-white">
+                Continue
+              </button>
+              <p className="text-[13px] py-2">
+                Don't have an account yet?{" "}
+                <a className="link text-[#002C74] mx-1">Sign up</a>
+              </p>
             </div>
-            <a href="#" className="text-sm left text-blue-800 hover:underline">
-              Forgot password?
-            </a>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="mt-4 flex w-full justify-center rounded-md bg-[#0097B2] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#4A6E7E] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Log in
-            </button>
           </div>
         </div>
       </div>
