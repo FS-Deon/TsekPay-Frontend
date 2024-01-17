c5725624f4fbea7bd6b8a32b9001f54487980acd
ags0522 <antoinette@fullsuite.ph>
2024-01-17 11:21:23 +0800

    minor update in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -195,7 +195,7 @@ function Sidebar() {
             </li>
             <li>
               <Link
-                to="#"
+                to="/login"
                 className="mt-12 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-800 dark:hover:bg-gray-700 group"
               >
                 <svg
