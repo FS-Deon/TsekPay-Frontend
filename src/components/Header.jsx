ebedf7251aabb7fc7d67e747462928b6b3cf74cd
angelo-artiaga <angelo.artiaga@gmail.com>
2024-01-19 13:35:44 +0800

    modify header & Dashboard
    fixed dashboard & header margin/padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,27 @@
 function Header() {
   return (
     <>
-      {/*greetings*/}
-      <div className="mx-2 mb-1 text-xl">January 8, 2024</div>
-      <div className="m-2 text-3xl font-bold">
-        <p>Good morning User</p>
-      </div>
-      {/*notices*/}
-      <div className="flex lg:flex-row flex-col">
-        <div className="flex flex-col lg:w-[30%]">
-          <div className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg flex flex-col items-center justify-center">
-            <h1 className="text-xl font-semibold mb-4">Date of last upload</h1>
-            <h1 className="text-[40px] font-black text-center">
-              December <br />
-              <span className="text-6xl">05,</span> <br /> 2023
-            </h1>
-            <p className="font-regular text-xl">16 Days Ago</p>
+      <div className="bg-[#4A6E7E] flex flex-row justify-between">
+        <div className="flex flex-row mx-3">
+          <img src="../Frame.png" alt="logo" />
+
+          <div className="mx-3 mt-3 flex flex-col hidden lg:block">
+            {/*greetings*/}
+            <div className="text-lg text-white font-bold">
+              <p>Good morning User</p>
+            </div>
+            <div className="text-lg text-[#AAE2EC]">January 8, 2024</div>
           </div>
-
-          <button className="mx-2 px-3 btn btn-outline border-2 border-gray-200">
-            Upload File
-          </button>
         </div>
-
-        <div className="m-2 p-3 border-2 lg:w-[70%] border-gray-200 border-solid rounded-lg  flex flex-col">
-          <h1>Important Dates</h1>
+        <div className="mx-5 my-1 flex flex-col">
+          <h3 className="text-[13px] font-regular text-white">Client</h3>
+          <select className="select select-bordered w-full max-w-xs px-20">
+            <option disabled selected>
+              Fullsuite
+            </option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select>
         </div>
       </div>
     </>
