dc45b86c784c27724cad802999fa388a57839e09
ags0522 <antoinette@fullsuite.ph>
2024-01-25 14:05:52 +0800

    Added new unloaded components

--- src/pages/TsekpayRun.jsx
+++ src/pages/TsekpayRun.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import NoRecord from "../components/NoRecord";
 
 function TsekpayRun() {
   return (
@@ -30,7 +31,6 @@ function TsekpayRun() {
           <div className="container w-[80%] flex flex-col">
             <h1 className="text-base font-bold">Period Covered</h1>
             <div className="flex flex-row">
-              {" "}
               <label className="form-control w-full max-w-xs mx-3">
                 <div className="label">
                   <span className="label-text">Date From</span>
@@ -64,11 +64,15 @@ function TsekpayRun() {
       </div>
 
       <div className="sm:ml-64 flex flex-col">
-        <h1 className="m-2 text-3xl font-bold font-black">Payroll File</h1>
-          <div className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg flex flex-row">
-           
+        <h1 className="m-5 px-5 text-l font-bold">Payroll File</h1>
+          <div className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg flex flex-row mx-10">
+            <NoRecord></NoRecord>
           </div>
       </div>
+
+      {/* <div className="sm:ml-64 flex flex-col">
+        <NoRecord></NoRecord>
+      </div> */}
     </>
   );
 }
