c60972dcf16982225ac55029dc8babc09cc7326b
angelo-artiaga <angelo.artiaga@gmail.com>
2024-01-25 15:45:53 +0800

    Modify select in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,8 +24,7 @@ function Header() {
         <div className="mx-5 my-1 flex flex-col">
           <h3 className="text-[13px] font-regular text-white">Client</h3>
           <select className="select select-bordered w-full max-w-xs px-20">
-            <option disabled>Fullsuite</option>
-            <option>Han Solo</option>
+            <option>Fullsuite</option>
             <option>Greedo</option>
           </select>
         </div>
