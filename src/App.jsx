44dd116f69db1f4f2e6a393d74ef8d72e605baee
angelo-artiaga <angelo.artiaga@gmail.com>
2024-01-30 10:35:12 +0800

    add payitems
    created pay itempage adding and list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "/src/pages/Login.jsx";
 import Dashboard from "/src/pages/Dashboard.jsx";
@@ -8,6 +7,7 @@ import NonRecurring from "/src/pages/NonRecurring.jsx";
 import ActivityLogs from "/src/pages/ActivityLogs.jsx";
 import SelectOrg from "./pages/SelectOrg.jsx";
 import ManageOrgs from "./pages/ManageOrgs.jsx";
+import PayItems from "./pages/PayItems.jsx";
 
 function App() {
   return (
@@ -19,12 +19,12 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/register" element={<Register />} />
-
             <Route path="/non-recurring" element={<NonRecurring />} />
             <Route path="/activity-logs" element={<ActivityLogs />} />
             <Route path="/select-orgnazation" element={<SelectOrg />} />
             <Route path="/tsekpay-run" element={<TsekpayRun />} />
             <Route path="/manage-organizations" element={<ManageOrgs />} />
+            <Route path="/pay-items" element={<PayItems />} />
           </Routes>
         </BrowserRouter>
       </div>
