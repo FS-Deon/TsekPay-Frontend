4cd63df4cd148bd12eeffdf2e5c1a56d1f80d102
Tony <antoinette@fullsuite.ph>
2024-02-01 17:49:33 +0800

    modal for add pay items

--- src/pages/PayItems.jsx
+++ src/pages/PayItems.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import AddPayItem from "../components/AddPayItem.jsx";
 
 function PayItems() {
   return (
@@ -9,76 +10,152 @@ function PayItems() {
         <Header />
         <div className="flex flex-row">
           <h1 className="m-5 px-5 text-3xl font-bold">Pay Items</h1>
-          {/* You can open the modal using document.getElementById('ID').showModal() method */}
-          <button
-            className="btn my-4"
-            onClick={() => document.getElementById("my_modal_4").showModal()}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-            Add Pay Items
-          </button>
-          <dialog id="my_modal_4" className="modal">
-            <div className="modal-box w-11/12 max-w-5xl">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">Click the button below to close</p>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button, it will close the modal */}
-                  <button className="btn">Close</button>
-                </form>
-              </div>
-            </div>
-          </dialog>
+
+          <AddPayItem></AddPayItem>
         </div>
-        <table class="m-4 p-3 border-2 border-gray-200 border-solid rounded-lg container ">
-          <thead>
-            <tr>
-              <th class="border-2 border-gray-200 border-solid font-bold text-lg py-2">
-                Earnings
-              </th>
-              <th class="border-2 border-gray-200 border-solid font-bold text-lg py-2">
-                Category
-              </th>
-              <th class="border-2 border-gray-200 border-solid  font-bold text-lg py-2">
-                Rate
-              </th>
-              <th class="border-2 border-gray-200 border-solid  font-bold text-lg py-2">
-                Paid To
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2">
-                Basic Pay
-              </td>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2"></td>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2"></td>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2"></td>
-            </tr>
-            <tr>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2">
-                Basic Pay
-              </td>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2"></td>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2"></td>
-              <td class="border-2 border-gray-200 border-solid text-lg py-2"></td>
-            </tr>
-          </tbody>
-        </table>
+
+        <div className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg flex flex-row mx-10">
+          <div className="overflow-x-auto">
+            <table className="table table-3xl">
+              <thead>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <th>Pay Item</th>
+                  <th>Pay Item Category</th>
+                  <th>Unit Hour|Day|Month</th>
+                  <th>Visible</th>
+                  <th>Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>Regular OT Pay</td>
+                  <td>Wages</td>
+                  <td>Hour</td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="green"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                      />
+                    </svg>
+                  </td>
+                </tr>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>Holiday Pay (All Souls Day)</td>
+                  <td>Wages</td>
+                  <td>Day</td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="green"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                      />
+                    </svg>
+                  </td>
+                </tr>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>Transportation Allowance</td>
+                  <td>Allowance</td>
+                  <td>Month</td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="green"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </td>
+                  <td>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                      />
+                    </svg>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   );
