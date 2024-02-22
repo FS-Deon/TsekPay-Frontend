ec6bb266eb3cc46a966038dea747375b2500ec06
angelo-artiaga <angelo.artiaga@gmail.com>
2024-02-22 11:12:01 +0800

    fix upload file button

--- src/pages/TsekpayRun.jsx
+++ src/pages/TsekpayRun.jsx
@@ -27,6 +27,7 @@ function TsekpayRun() {
     reader.readAsBinaryString(e.target.files[0]);
     reader.onload = (e) => {
       const data = e.target.result;
+
       const workbook = XLSX.read(data, { type: "binary" });
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
@@ -61,23 +62,42 @@ function TsekpayRun() {
 
       <form className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg flex flex-row mx-10">
         <div className="flex flex-col container w-[25%] m-5">
-          <label className="form-control w-full max-w-xs my-2">
-            <div className="label">
-              <span className="label-text">Upload Payroll File</span>
-            </div>
+          <label
+            htmlFor="uploadFile1"
+            className="btn bg-[#426E80] btn-wide shadow-md px-4 m-2 my-2 text-white hover:bg-[#AAE2EC] hover:text-[#426E80]"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 mr-2 fill-white inline"
+              viewBox="0 0 32 32"
+            >
+              <path
+                d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+                data-original="#000000"
+              />
+              <path
+                d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+                data-original="#000000"
+              />
+            </svg>
+            Upload Image
             <input
               type="file"
               accept=".xlsx, .xls, .csv"
               onChange={handleFileUpload}
-              className="file:px-3 w-[95%] file-input file-input-bordered w-full max-w-xs mx-2 file:bg-[#426E80]"
+              id="uploadFile1"
+              className="hidden"
+              name="csvFile"
             />
           </label>
+
           <button
             className="btn bg-[#5C9CB7] btn-wide shadow-md px-5 m-2  "
             disabled="disabled"
           >
             Payslip PDF Format
           </button>
+
           <button
             className="btn bg-[#5C9CB7] btn-wide shadow-md px-4 m-2 "
             disabled="disabled"
@@ -142,10 +162,6 @@ function TsekpayRun() {
                       <input
                         type="checkbox"
                         className="checkbox bg-[#fff] my-2"
-                        checked={selectAll}
-                        onChange={() => {
-                          setSelectAll((current) => !current);
-                        }}
                       />
                     </label>
                   </th>
@@ -179,18 +195,20 @@ function TsekpayRun() {
         )}
       </div>
       {selectedRow && (
-        <div className="md:ml-64 flex flex-col">
+        <div className=" flex flex-col">
           <div className="m-2 border-2 border-gray-200 border-solid rounded-lg flex flex-col mx-10">
             <div className="bg-[#4A6E7E] text-white rounded-t-lg w-full flex flex-col">
-              <h1 className="font-bold text-2xl py-3 mx-3">{selectedRow[2]}</h1>
+              <h1 className="font-bold text-2xl py-3 mx-3">
+                {selectedRow["Name"]}
+              </h1>
               <div className="flex flex-col lg:flex-row my-3">
                 <h2 className="mx-4">
                   <strong>Email: </strong>
-                  {selectedRow[18]}
+                  {selectedRow["Email Address"]}
                 </h2>
                 <h2 className="mx-4">
                   <strong>Tax Number: </strong>
-                  {selectedRow[6]}
+                  {selectedRow["Tin"]}
                 </h2>
                 <h2 className="mx-4">
                   <strong>Ordinary Rate: </strong>000000000000
@@ -208,14 +226,59 @@ function TsekpayRun() {
                 <hr className="mt-1"></hr>
                 <div className="flex flex-row justify-between">
                   <h1 className="mx-3 mt-3">Basic Pay</h1>
-                  <h1 className="mx-3 mt-3">9,000</h1>
+                  <h1 className="mx-3 mt-3">{selectedRow["Basic Pay"]}</h1>
+                </div>
+                <hr className="mt-1"></hr>
+                <div className="flex flex-row justify-between">
+                  <h1 className="mx-3 mt-3">
+                    Clothing and Laundry Allowance (de minimis)
+                  </h1>
+                  <h1 className="mx-3 mt-3">
+                    {selectedRow["Clothing and Laundry Allowance (de minimis)"]}
+                  </h1>
+                </div>
+                <hr className="mt-1"></hr>
+                <div className="flex flex-row justify-between">
+                  <h1 className="mx-3 mt-3">Meal Allowance (taxable)</h1>
+                  <h1 className="mx-3 mt-3">
+                    {selectedRow["Meal Allowance (taxable)"]}
+                  </h1>
+                </div>
+                <hr className="mt-1"></hr>
+                <div className="flex flex-row justify-between">
+                  <h1 className="mx-3 mt-3">Medical Allowance (De minimis)</h1>
+                  <h1 className="mx-3 mt-3">
+                    {selectedRow["Medical Allowance (De minimis)"]}
+                  </h1>
+                </div>
+                <hr className="mt-1"></hr>
+                <div className="flex flex-row justify-between">
+                  <h1 className="mx-3 mt-3">Medical Allowance (Taxable)</h1>
+                  <h1 className="mx-3 mt-3">
+                    {selectedRow["Medical Allowance (Taxable)"]}
+                  </h1>
+                </div>
+                <hr className="mt-1"></hr>
+                <div className="flex flex-row justify-between">
+                  <h1 className="mx-3 mt-3">Rice Allowance (De minimis)</h1>
+                  <h1 className="mx-3 mt-3">
+                    {selectedRow["RIce Allowance (De minimis)"]}
+                  </h1>
                 </div>
                 <hr className="mt-1"></hr>
                 <div className="flex flex-row justify-between">
                   <h1 className="font-bold mx-3 mt-3">Total Earnings</h1>
-                  <h1 className="mx-3 mt-3">9,000</h1>
+                  <h1 className="mx-3 mt-3">
+                    {selectedRow["Basic Pay"] +
+                      selectedRow["Meal Allowance (taxable)"] +
+                      selectedRow["Medical Allowance (De minimis)"] +
+                      selectedRow["Medical Allowance (Taxable)"] +
+                      selectedRow[
+                        "Clothing and Laundry Allowance (de minimis)"
+                      ] +
+                      selectedRow["RIce Allowance (De minimis)"]}
+                  </h1>
                 </div>
-                <hr className="mt-1"></hr>
 
                 <hr className="mt-1"></hr>
                 <div className="flex flex-row justify-between">
@@ -248,7 +311,7 @@ function TsekpayRun() {
                       <th>Pay Items</th>
                       <th>Rate</th>
                       <th>QTY</th>
-                      <th>Ammount</th>
+                      <th>Amount</th>
                       <th></th>
                     </tr>
                   </thead>
